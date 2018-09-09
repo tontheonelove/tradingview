38e4300bebf63ecc5c5a0e2c2d5cd80967144b8c
tontheonelove <tonlove5233@gmail.com>
2018-09-09 17:58:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<style>
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  grid-gap: 10px;
+  background-color: #2196F3;
+  padding: 10px;
+}
+.grid-container > div {
+  background-color: rgba(255, 255, 255, 0.8);
+  text-align: center;
+  padding: 20px 0;
+  font-size: 30px;
+}
+.item1 {
+  grid-column-start: 1;
+  grid-column-end: 3;
+}
+</style>
 </head>
 <body>
 
-<div class="container-fluid">
-  <h2>Three Equal Columns</h2>
-  <p>Use the .col class on a specified number of elements and Bootstrap will recognize how many elements there are (and create equal-width columns). In the example below, we use three col elements, which gets a width of 33.33% each.</p>
-  <div class="row">
-    <div class="col bg-success"><!-- TradingView Widget BEGIN -->
+<h1>Grid Lines</h1>
+
+<div class="grid-container">
+  <div class="item1"><!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
   <div id="tradingview_5ac5d"></div>
   <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/BTCUSDT/" rel="noopener" target="_blank"><span class="blue-text">BTCUSDT chart</span></a> by TradingView</div>
@@ -23,8 +34,8 @@
   <script type="text/javascript">
   new TradingView.widget(
   {
-  "width": 980,
-  "height": 610,
+  "width": 600,
+  "height": 300,
   "symbol": "BTCUSDT",
   "interval": "5",
   "timezone": "Asia/Bangkok",
@@ -44,7 +55,9 @@
   </script>
 </div>
 <!-- TradingView Widget END --></div>
-    <div class="col bg-warning"><!-- TradingView Widget BEGIN -->
+
+
+  <div class="item2"><!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
   <div id="tradingview_5ac5d"></div>
   <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/BTCUSDT/" rel="noopener" target="_blank"><span class="blue-text">BTCUSDT chart</span></a> by TradingView</div>
@@ -52,8 +65,8 @@
   <script type="text/javascript">
   new TradingView.widget(
   {
-  "width": 980,
-  "height": 610,
+  "width": 600,
+  "height": 300,
   "symbol": "BTCUSDT",
   "interval": "5",
   "timezone": "Asia/Bangkok",
@@ -73,37 +86,15 @@
   </script>
 </div>
 <!-- TradingView Widget END --></div>
-    <div class="col bg-success"><!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container">
-  <div id="tradingview_5ac5d"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/BTCUSDT/" rel="noopener" target="_blank"><span class="blue-text">BTCUSDT chart</span></a> by TradingView</div>
-  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-  <script type="text/javascript">
-  new TradingView.widget(
-  {
-  "width": 980,
-  "height": 610,
-  "symbol": "BTCUSDT",
-  "interval": "5",
-  "timezone": "Asia/Bangkok",
-  "theme": "Dark",
-  "style": "8",
-  "locale": "en",
-  "toolbar_bg": "#f1f3f6",
-  "enable_publishing": false,
-  "allow_symbol_change": true,
-  "studies": [
-    "MASimple@tv-basicstudies",
-    "StochasticRSI@tv-basicstudies"
-  ],
-  "container_id": "tradingview_5ac5d"
-}
-  );
-  </script>
+  <div class="item3">3</div>  
+  <div class="item4">4</div>
+  <div class="item5">5</div>
+  <div class="item6">6</div>
+  <div class="item7">7</div>
+  <div class="item8">8</div>  
 </div>
-<!-- TradingView Widget END --></div>
-  </div>
-</div>
+
+<p>You can refer to line numbers when placing grid items.</p>
 
 </body>
 </html>
