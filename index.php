7a3e7607446b31958d6c2fa52d43deffaee23deb
tontheonelove <tonlove5233@gmail.com>
2018-09-09 17:39:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,43 +4,92 @@
       <base href = "https://www.tutorialspoint.com/" />
       
    </head>
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="bitcoin" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
 
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="ethereum" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
+<div class="grid">
+  <section class="head">
+    <h1>4x1 Grid Landing Page Template</h1>		
+  </section>
 
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="bitcoin-cash" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
+  <section class="title">
+    <h2>Title of Section</h2>		
+  </section>
 
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="bitcoin-gold" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
+  <section class="body">
+    <ul>
+      <li class="col span_1_of_4">
+        <div class="product">
+          <img src="" border="0" />
+          <h3>Product Name</h3>
+          <h4>Price</h4>
+          <h5>Cashback</h5>
+        </div>
+      </li>
+      <li class="col span_1_of_4">
+        <div class="product">
+          <img src="" alt="" />
+          <h3>Product Name</h3>
+          <h4>Price</h4>
+          <h5>Cashback</h5>
+        </div>
+      </li>
+      <li class="col span_1_of_4">
+        <div class="product">
+          <img src="" alt="" />
+          <h3>Product Name</h3>
+          <h4>Price</h4>
+          <h5>Cashback</h5>
+        </div>
+      </li>
+      <li class="col span_1_of_4">
+        <div class="product">
+          <img src="" alt="" />
+          <h3>Product Name</h3>
+          <h4>Price</h4>
+          <h5>Cashback</h5>
+        </div>
+      </li>
+    </ul>
+    <ul>
+      <li class="col span_1_of_4">
+        <div class="product">
+          <img src="" border="0" />
+          <h3>Product Name</h3>
+          <h4>Price</h4>
+          <h5>Cashback</h5>
+        </div>
+      </li>
+      <li class="col span_1_of_4">
+        <div class="product">
+          <img src="" alt="" />
+          <h3>Product Name</h3>
+          <h4>Price</h4>
+          <h5>Cashback</h5>
+        </div>
+      </li>
+      <li class="col span_1_of_4">
+        <div class="product">
+          <img src="" alt="" />
+          <h3>Product Name</h3>
+          <h4>Price</h4>
+          <h5>Cashback</h5>
+        </div>
+      </li>
+      <li class="col span_1_of_4">
+        <div class="product">
+          <img src="" alt="" />
+          <h3>Product Name</h3>
+          <h4>Price</h4>
+          <h5>Cashback</h5>
+        </div>
+      </li>
+    </ul>
+  </section>
+  <section class="footer">
+    <h2>Footer</h2>	
+  </section>
+</div>
 
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="dash" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
-
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="zcash" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
-
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="omisego" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
-
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="ripple" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
-
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="augur" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
-
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="litecoin" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
-
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="eos" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
-
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="iota" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
-
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="neo" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
-
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="ethereum-classic" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
-
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="gnosis-gno" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
-
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="qtum" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
 
 
 
-
-
- <script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="zcoin" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
-
-<script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></script><div class="coinmarketcap-currency-widget" data-currency="everex" data-base="THB" data-secondary="BTC" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-stats="USD" data-statsticker="false"></div>
 <html>
