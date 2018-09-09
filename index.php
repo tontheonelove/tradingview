fc4514320e39258ef21de655ac88a7f2fc9ccc35
tontheonelove <tonlove5233@gmail.com>
2018-09-09 17:49:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
-   <head>
-      <title>ราคาCryptoโลก</title>
-      <base href = "https://www.tutorialspoint.com/" />
-      
-   </head>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-<div class="grid">
-  <section class="head">
-    
-     <!-- TradingView Widget BEGIN -->
+<div class="container-fluid">
+  <h2>Three Equal Columns</h2>
+  <p>Use the .col class on a specified number of elements and Bootstrap will recognize how many elements there are (and create equal-width columns). In the example below, we use three col elements, which gets a width of 33.33% each.</p>
+  <div class="row">
+    <div class="col <!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
   <div id="tradingview_5ac5d"></div>
   <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/BTCUSDT/" rel="noopener" target="_blank"><span class="blue-text">BTCUSDT chart</span></a> by TradingView</div>
@@ -36,16 +43,8 @@
   );
   </script>
 </div>
-<!-- TradingView Widget END -->
-     
-    
-     
-  </section>
-
-  <section class="title">
-   
-     
-     <!-- TradingView Widget BEGIN -->
+<!-- TradingView Widget END -->">.col</div>
+    <div class="col <!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
   <div id="tradingview_5ac5d"></div>
   <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/BTCUSDT/" rel="noopener" target="_blank"><span class="blue-text">BTCUSDT chart</span></a> by TradingView</div>
@@ -73,102 +72,38 @@
   );
   </script>
 </div>
-<!-- TradingView Widget END -->
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-  </section>
-
-  <section class="body">
-    <ul>
-      <li class="col span_1_of_4">
-        <div class="product">
-          <img src="" border="0" />
-          <h3>Product Name</h3>
-          <h4>Price</h4>
-          <h5>Cashback</h5>
-        </div>
-      </li>
-      <li class="col span_1_of_4">
-        <div class="product">
-          <img src="" alt="" />
-          <h3>Product Name</h3>
-          <h4>Price</h4>
-          <h5>Cashback</h5>
-        </div>
-      </li>
-      <li class="col span_1_of_4">
-        <div class="product">
-          <img src="" alt="" />
-          <h3>Product Name</h3>
-          <h4>Price</h4>
-          <h5>Cashback</h5>
-        </div>
-      </li>
-      <li class="col span_1_of_4">
-        <div class="product">
-          <img src="" alt="" />
-          <h3>Product Name</h3>
-          <h4>Price</h4>
-          <h5>Cashback</h5>
-        </div>
-      </li>
-    </ul>
-    <ul>
-      <li class="col span_1_of_4">
-        <div class="product">
-          <img src="" border="0" />
-          <h3>Product Name</h3>
-          <h4>Price</h4>
-          <h5>Cashback</h5>
-        </div>
-      </li>
-      <li class="col span_1_of_4">
-        <div class="product">
-          <img src="" alt="" />
-          <h3>Product Name</h3>
-          <h4>Price</h4>
-          <h5>Cashback</h5>
-        </div>
-      </li>
-      <li class="col span_1_of_4">
-        <div class="product">
-          <img src="" alt="" />
-          <h3>Product Name</h3>
-          <h4>Price</h4>
-          <h5>Cashback</h5>
-        </div>
-      </li>
-      <li class="col span_1_of_4">
-        <div class="product">
-          <img src="" alt="" />
-          <h3>Product Name</h3>
-          <h4>Price</h4>
-          <h5>Cashback</h5>
-        </div>
-      </li>
-    </ul>
-  </section>
-  <section class="footer">
-    <h2>Footer</h2>	
-  </section>
+<!-- TradingView Widget END -->">.col</div>
+    <div class="col <!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div id="tradingview_5ac5d"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/BTCUSDT/" rel="noopener" target="_blank"><span class="blue-text">BTCUSDT chart</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.widget(
+  {
+  "width": 980,
+  "height": 610,
+  "symbol": "BTCUSDT",
+  "interval": "5",
+  "timezone": "Asia/Bangkok",
+  "theme": "Dark",
+  "style": "8",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "allow_symbol_change": true,
+  "studies": [
+    "MASimple@tv-basicstudies",
+    "StochasticRSI@tv-basicstudies"
+  ],
+  "container_id": "tradingview_5ac5d"
+}
+  );
+  </script>
+</div>
+<!-- TradingView Widget END -->">.col</div>
+  </div>
 </div>
 
-
-
-
-<html>
+</body>
+</html>
