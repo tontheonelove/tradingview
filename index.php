a5bcc163c9468a580d0ce2eccd41c7ea0491cbd0
tontheonelove <tonlove5233@gmail.com>
2018-09-19 08:42:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,4 +14,12 @@
 
    </div>
 
+<div align="center">
+<iframe frameborder="0" scrolling="no" height="253" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=7,8,8830,8836,166&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;"> </a></span></span></div>
+
+ </div>
+
+
+
+
 </html>
