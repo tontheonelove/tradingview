ae40937c55b07b4e868c37570b0a7b3b2066e0f6
tontheonelove <tonlove5233@gmail.com>
2018-09-19 12:22:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <iframe src="https://th.widgets.investing.com/live-currency-cross-rates?theme=darkTheme&roundedCorners=true&pairs=1,3,2,4,7,5,8,6,9,10,49,11" width="100%" height="100%" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0"></iframe>
-<div align="left">
+<div align="center">
 <iframe frameborder="0" scrolling="no" height="274" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=1,2,3,4,5,6&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;">
 </div>
 <iframe frameborder="0" scrolling="no" height="274" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=7,8,9,10,11,12&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;">
