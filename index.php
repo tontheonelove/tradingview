ca5f1c619076d987e3548f4faa93a36807913a39
tontheonelove <tonlove5233@gmail.com>
2018-09-19 12:25:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 <iframe frameborder="0" scrolling="no" height="274" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=1,2,3,4,5,6&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;">
 </div>
 <iframe frameborder="0" scrolling="no" height="274" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=7,8,9,10,11,12&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;">
-  
+<iframe frameborder="0" scrolling="no" height="274" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=49,8830,8836,8849,166,178&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;">
   
   
