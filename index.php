7e17b488e05192e50a56aec7e6a8bd84f88451c8
tontheonelove <tonlove5233@gmail.com>
2018-09-19 08:38:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
   <div align="center">
 
   
-  <iframe frameborder="0" scrolling="no" height="274" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=1,2,3,4,5,6&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;"> rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;" class="underline_link">Investing.com ประเทศไทย</a></span></span></div>
+  <iframe frameborder="0" scrolling="no" height="274" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=1,2,3,4,5,6&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;"> rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;" </a></span></span></div>
   
   
   </div>
 
 <div align="center">
   
-<iframe frameborder="0" scrolling="no" height="253" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=9,10,11,12,49&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;"> การวิเคราะห์ทางเศรษฐกิจนี้ควบคุมดูแลระบบการทำงานโดย<a href="https://th.investing.com/" rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;" class="underline_link">Investing.com ประเทศไทย</a></span></span></div>
+<iframe frameborder="0" scrolling="no" height="253" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=9,10,11,12,49&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;">  rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;"</a></span></span></div>
 
    </div>
 
