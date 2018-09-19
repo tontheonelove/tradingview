abdfc1930c6bdae1ece2c57d0fdafc29aad839bf
tontheonelove <tonlove5233@gmail.com>
2018-09-19 11:14:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,6 @@
 <!DOCTYPE html>
 <html>
   
-  
- 
-  <div align="center">
-
-  
-  <iframe frameborder="0" scrolling="no" height="274" width="800" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=1,2,3,4,5,6&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;"> </a></span></span></div>
-  
-  
-  </div>
-
-<div align="center">
-  
-<iframe frameborder="0" scrolling="no" height="253" width="800" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=9,10,11,12,49&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;"></a></span></span></div>
-
-   </div>
-
-<div align="center">
-<iframe frameborder="0" scrolling="no" height="253" width="800" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=7,8,8830,8836,166&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;"> </a></span></span></div>
-
- </div>
-
-
   <iframe src="https://th.widgets.investing.com/live-currency-cross-rates?theme=darkTheme&roundedCorners=true&pairs=1,3,2,4,7,5,8,6,9,10,49,11" width="100%" height="100%" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0"></iframe><div class="poweredBy" style="font-family: Arial, Helvetica, sans-serif;">สนับสนุนโดย <a href="https://th.investing.com?utm_source=WMT&amp;utm_medium=referral&amp;utm_campaign=LIVE_CURRENCY_X_RATES&amp;utm_content=Footer%20Link" target="_blank" rel="nofollow">Investing.com</a></div>
-
 
 </html>
