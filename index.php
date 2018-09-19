eef369084d5943081e442f9e019f81c2e61475c6
tontheonelove <tonlove5233@gmail.com>
2018-09-19 12:06:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
   <iframe src="https://th.widgets.investing.com/live-currency-cross-rates?theme=darkTheme&roundedCorners=true&pairs=1,3,2,4,7,5,8,6,9,10,49,11" width="100%" height="100%" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0"></iframe>
-  <div align="center">
+  <div align="left">
  
   <iframe frameborder="0" scrolling="no" height="274" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=1,2,3,4,5,6&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;"> การวิเคราะห์ทางเศรษฐกิจนี้ควบคุมดูแลระบบการทำงานโดย<a href="https://th.investing.com/" rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;" class="underline_link">Investing.com ประเทศไทย</a></span></span></div>
   
