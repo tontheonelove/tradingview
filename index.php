71bc67b51f9c002b102df2e741ee366ec203dbf1
tontheonelove <tonlove5233@gmail.com>
2019-10-20 00:27:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,24 +21,7 @@
 </div>
 <!-- TradingView Widget END --></td>
          
-<!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container">
-  <div class="tradingview-widget-container__widget"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/FX-EURUSD/technicals/" rel="noopener" target="_blank"><span class="blue-text">Technical Analysis for EURUSD</span></a> by TradingView</div>
-  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js" async>
-  {
-  "showIntervalTabs": true,
-  "width": 425,
-  "colorTheme": "light",
-  "isTransparent": false,
-  "locale": "en",
-  "symbol": "FX:EURUSD",
-  "interval": "1h",
-  "height": 450
-}
-  </script>
-</div>
-<!-- TradingView Widget END -->
+
             
       <td><!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
