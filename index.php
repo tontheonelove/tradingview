dccfd42b57078b30130d30a56b0e9ba605004f4e
tontheonelove <tonlove5233@gmail.com>
2019-10-20 00:11:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,13 @@
-<!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container">
-  <div class="tradingview-widget-container__widget"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/FX-GBPUSD/technicals/" rel="noopener" target="_blank"><span class="blue-text">Technical Analysis for GBPUSD</span></a> by TradingView</div>
-  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js" async>
-  {
-  "showIntervalTabs": true,
-  "width": 425,
-  "colorTheme": "light",
-  "isTransparent": false,
-  "locale": "en",
-  "symbol": "FX:GBPUSD",
-  "interval": "1h",
-  "height": 450
-}
-  </script>
-</div>
-<!-- TradingView Widget END -->
-
-<!-- TradingView Widget BEGIN -->
+<table>
+  <thead>
+    <tr>
+      <th>Month</th>
+      <th>Savings</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
   <div class="tradingview-widget-container__widget"></div>
   <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/FX-USDJPY/technicals/" rel="noopener" target="_blank"><span class="blue-text">Technical Analysis for USDJPY</span></a> by TradingView</div>
@@ -34,7 +24,36 @@
 }
   </script>
 </div>
-<!-- TradingView Widget END -->
-
-
-
+<!-- TradingView Widget END --></td>
+      
+      <td><!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/FX-GBPUSD/technicals/" rel="noopener" target="_blank"><span class="blue-text">Technical Analysis for GBPUSD</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js" async>
+  {
+  "showIntervalTabs": true,
+  "width": 425,
+  "colorTheme": "light",
+  "isTransparent": false,
+  "locale": "en",
+  "symbol": "FX:GBPUSD",
+  "interval": "1h",
+  "height": 450
+}
+  </script>
+</div>
+<!-- TradingView Widget END --></td>
+    </tr>
+    <tr>
+      <td>February</td>
+      <td>$80</td>
+    </tr>
+  </tbody>
+  <tfoot>
+    <tr>
+      <td>Sum</td>
+      <td>$180</td>
+    </tr>
+  </tfoot>
+</table>
