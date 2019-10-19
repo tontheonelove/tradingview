2f1456ea076f857f2d306a9bae7202401efcb8f5
tontheonelove <tonlove5233@gmail.com>
2019-10-19 23:59:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
-<iframe src="https://th.widgets.investing.com/live-currency-cross-rates?theme=darkTheme&roundedCorners=true&pairs=1,3,2,4,7,5,8,6,9,10,49,11" width="100%" height="100%" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0"></iframe>
-<div align="center">
-<iframe frameborder="0" scrolling="no" height="274" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=1,2,3,4,5,6&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;">
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/FX-GBPUSD/technicals/" rel="noopener" target="_blank"><span class="blue-text">Technical Analysis for GBPUSD</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js" async>
+  {
+  "showIntervalTabs": true,
+  "width": 425,
+  "colorTheme": "light",
+  "isTransparent": false,
+  "locale": "en",
+  "symbol": "FX:GBPUSD",
+  "interval": "1h",
+  "height": 450
+}
+  </script>
 </div>
-<iframe frameborder="0" scrolling="no" height="274" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=7,8,9,10,11,12&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;">
-<iframe frameborder="0" scrolling="no" height="274" width="505" allowtransparency="true" marginwidth="0" marginheight="0" src="https://ssltools.forexprostools.com/technical_summary.php?pairs=49,8830,8836,8849,166,178&curr-name-color=%230059B0&fields=5m,15m,1h,1d&force_lang=53"></iframe><br /><div style="width:505px;"><span style="float:left"><span style="font-size: 11px;color: #333333;text-decoration: none;">
-  
-  
+<!-- TradingView Widget END -->
