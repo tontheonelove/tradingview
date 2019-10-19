973b9b95b72cfb7b1b11eb6cd3d2ca338f2cd8b4
tontheonelove <tonlove5233@gmail.com>
2019-10-20 00:29:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -43,6 +43,47 @@
 <!-- TradingView Widget END --></td>
       
       
+      <td><!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/FX-USDJPY/technicals/" rel="noopener" target="_blank"><span class="blue-text">Technical Analysis for USDJPY</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js" async>
+  {
+  "showIntervalTabs": true,
+  "width": 425,
+  "colorTheme": "light",
+  "isTransparent": false,
+  "locale": "en",
+  "symbol": "FX:USDJPY",
+  "interval": "1h",
+  "height": 450
+}
+  </script>
+</div>
+<!-- TradingView Widget END --></td>
+         
+
+            
+      <td><!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/FX-GBPUSD/technicals/" rel="noopener" target="_blank"><span class="blue-text">Technical Analysis for GBPUSD</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js" async>
+  {
+  "showIntervalTabs": true,
+  "width": 425,
+  "colorTheme": "light",
+  "isTransparent": false,
+  "locale": "en",
+  "symbol": "FX:GBPUSD",
+  "interval": "1h",
+  "height": 450
+}
+  </script>
+</div>
+<!-- TradingView Widget END --></td>
+      
+      
           
       
       
